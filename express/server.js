e8a1c6d850c79b0df45e07d08d6e8be0e1295b6d
ThitipatP <thitipat.pengruean@microchip.com>
2022-11-10 10:19:09 +0700

    test api

--- express/server.js
+++ express/server.js
@@ -13,9 +13,10 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/testV1', (req, res) => res.json({ value : "Test" }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/api', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
